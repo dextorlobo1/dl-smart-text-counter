0abdf4a12d38776baa6f399645f2bdf962ef616c
dextorlobo1 <dextorlobo@gmail.com>
2025-12-17 18:09:08 +0530

    Update dl-stc.js

--- assets/js/dl-stc.js
+++ assets/js/dl-stc.js
@@ -1,16 +1,16 @@
-jQuery(document).ready(function($) {
-    $("#tc-counter-form").on("submit", function(e) {
-        e.preventDefault();
-        var text = $("#tc-text").val().trim();
+jQuery(document).ready(function(jQuery) {
+	jQuery("#tc-counter-form").on("submit", function(e) {
+		e.preventDefault();
+		var text = jQuery("#tc-text").val().trim();
 
-        var charCount = text.length;
-        var wordCount = text ? text.split(/\s+/).length : 0;
-        var sentenceCount = text ? text.split(/[.!?]+/).filter(Boolean).length : 0;
+		var charCount = text.length;
+		var wordCount = text ? text.split(/\s+/).length : 0;
+		var sentenceCount = text ? text.split(/[.!?]+/).filter(Boolean).length : 0;
 
-        $("#tc-characters").text(charCount);
-        $("#tc-words").text(wordCount);
-        $("#tc-sentences").text(sentenceCount);
-        
-        $("#tc-result").slideDown();
-    });
+		jQuery("#tc-characters").text(charCount);
+		jQuery("#tc-words").text(wordCount);
+		jQuery("#tc-sentences").text(sentenceCount);
+		
+		jQuery("#tc-result").slideDown();
+	});
 });
